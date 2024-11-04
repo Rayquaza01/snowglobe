43953c6c9604737a3149c309f9977e75813a3ade
Arnaught <rayquaza01@outlook.com>
2024-11-03 20:29:17 -0500

    Initial commit

---
+++ src/main.lua
@@ -0,0 +1,14 @@
+--[[pod_format="raw",created="2024-03-15 13:58:36",modified="2024-11-04 01:27:25",revision=31]]
+-- name of cart v1.0
+-- by Arnaught
+
+function _init()
+end
+
+function _update()
+end
+
+function _draw()
+	cls()
+	print("Hello, world!")
+end
